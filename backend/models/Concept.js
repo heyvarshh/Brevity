e6bb781e32f864b86b1d32e2b1ab6c23eeca9610
Varsha M <varshha.mallesham@gmail.com>
2026-04-18 11:57:49 +0530

    Update Concept.js

--- backend/models/Concept.js
+++ backend/models/Concept.js
@@ -1,27 +1,15 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/db');
-
-const Concept = sequelize.define('Concept', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-
-  name: {
-    type: DataTypes.STRING
-  },
-
-  description: {
-    type: DataTypes.TEXT
-  },
-
-  session_id: {
-    type: DataTypes.INTEGER
-  }
-}, {
-  tableName: 'concepts',
-  timestamps: false
-});
-
-module.exports = Concept;
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define("Concept", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    session_id: DataTypes.INTEGER
+  }, {
+    tableName: "concepts",
+    timestamps: false
+  });
+};
