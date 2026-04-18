7a66cbe9bfcf452618b9897143f9b2cf48f75344
Varsha M <varshha.mallesham@gmail.com>
2026-04-18 11:56:29 +0530

    Update TranscriptChunk.js

--- backend/models/TranscriptChunk.js
+++ backend/models/TranscriptChunk.js
@@ -5,26 +5,11 @@ module.exports = (sequelize, DataTypes) => {
       primaryKey: true,
       autoIncrement: true
     },
-
-    content: {
-      type: DataTypes.TEXT
-    },
-
-    start_time: {
-      type: DataTypes.FLOAT
-    },
-
-    end_time: {
-      type: DataTypes.FLOAT
-    },
-
-    speaker: {
-      type: DataTypes.STRING
-    },
-
-    session_id: {
-      type: DataTypes.INTEGER
-    }
+    content: DataTypes.TEXT,
+    start_time: DataTypes.FLOAT,
+    end_time: DataTypes.FLOAT,
+    speaker: DataTypes.STRING,
+    session_id: DataTypes.INTEGER
   }, {
     tableName: "transcript_chunks",
     timestamps: false
